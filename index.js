5603d656b7668999c85293d5960d9023b39e8572
yasir7341 <yasirabdulrahman75@gmail.com>
2022-08-18 00:48:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+multiply = 'num1 * num2'
+num1 = '2'
+num2 = '32'
+
+random = Math.floor(Math.random() * 10) + 1;
+
+mod = `num3 / num4`
+num3 = `28`
+num4 = `2`
+
+max = Math.max(Number)
+Number = ('2' , '12' , '16' , '10' , '20')
